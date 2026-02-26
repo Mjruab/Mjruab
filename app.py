1152ae9515d1b8175599702ed2318ba8a3c45eb7
Mjruab <mjruab@eafit.edu.co>
2026-02-26 07:12:25 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,10 @@
 import streamlit as st 
+from PIL import Image
 
 st.title("App1")
+
+st.header("Este espacio es para el desarrollo de mis aplicaciones para Interfaces Multimodales.")
+st.write("Fácilmente se realiza Back-End y Front-End.")
+image = Image.open('Multimodal1.jpg')
+
+st.image(image, caption='Interfaces Multimodales')
