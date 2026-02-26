24d495b7f812ee98fb2f3e4f09553c1bf66a25cb
Mjruab <mjruab@eafit.edu.co>
2026-02-26 07:15:56 -0500

    Update app.py

--- app.py
+++ app.py
@@ -8,3 +8,7 @@ st.write("Fácilmente se realiza Back-End y Front-End.")
 image = Image.open('Multimodal1.jpg')
 
 st.image(image, caption='Interfaces Multimodales')
+
+texto = st.text_input('Escribe algo', 'Este es mi texto')
+st.write('El texto escrito es:', texto)
+
