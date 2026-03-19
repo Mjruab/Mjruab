ada96dd83e5a59f75d7f3861600818543552b041
Mjruab <mjruab@eafit.edu.co>
2026-03-19 15:03:35 -0500

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ st.title("Mi primera app!")
 
 st.header("Este espacio es para el desarrollo de mis aplicaciones para Interfaces Multimodales.")
 st.write("Fácilmente se realiza Back-End y Front-End.")
-image = Image.open('Multimodal1.jpg')
+image = Image.open('Interacción futura con interfaces multimodales.png')
 
 st.image(image, caption='Interfaces Multimodales')
 
